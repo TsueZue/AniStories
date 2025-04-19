d1e75f19e0c5c670f9d82faa448bd21d4d5b8a1e
tsue <tsuezue@gmail.com>
2025-04-19 01:12:24 -0300

    a

--- js/ui.js
+++ js/ui.js
@@ -2,9 +2,16 @@ const randomBtn = document.querySelector('.random')
 const main = document.querySelector('main')
 const animePage = document.querySelector('.animePage')
 
+const arrowbackBtn = document.querySelector('.arrowBack')
+
 randomBtn.addEventListener('click', function() {
 
     main.classList.add('inative')
     animePage.classList.remove('inative')
     
+})
+
+arrowbackBtn.addEventListener('click', () => {
+    main.classList.toggle('inative')
+    animePage.classList.toggle('inative')
 })
