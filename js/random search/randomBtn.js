d366b6af270f169dbc7bd8797c318af369878aeb
tsue <tsuezue@gmail.com>
2025-05-17 17:42:27 -0300

    adicionado pwa

--- js/random search/randomBtn.js
+++ js/random search/randomBtn.js
@@ -11,3 +11,10 @@ arrowbackBtn.addEventListener('click', () => {
     main.classList.toggle('inative')
     animePage.classList.toggle('inative')
 })
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(() => console.log('Service Worker registrado!'))
+      .catch((error) => console.error('Erro ao registrar o SW:', error));
+  }
+  
---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker instalado');
+    self.skipWaiting();
+  });
+  
+  self.addEventListener('activate', (event) => {
+    console.log('Service Worker ativado');
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    // Deixa o navegador tratar as requisições normalmente
+  });
+  
